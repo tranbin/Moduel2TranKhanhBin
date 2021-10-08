ef3d2a46b096b6d251a21e197e14632cfae53c35
tranbin <trankhanhbin1271997@gmail.com>
2021-10-08 22:56:37 +0700

    update baitap+thuchanh accessmodifier

---
+++ src/ss5_access_staticMethod_staticProperty/bai_tap/Circle/Circle.java
@@ -0,0 +1,20 @@
+package ss5_access_staticMethod_staticProperty.bai_tap.Circle;
+
+public class Circle {
+    private double radius = 2.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return this.radius ;
+    }
+    public double getArea(){
+        return getRadius()*3.14;
+    }
+
+}
---
+++ src/ss5_access_staticMethod_staticProperty/bai_tap/Circle/TestCircle.java
@@ -0,0 +1,9 @@
+package ss5_access_staticMethod_staticProperty.bai_tap.Circle;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.getRadius();
+        System.out.println("Area : " + circle.getArea());
+    }
+}
