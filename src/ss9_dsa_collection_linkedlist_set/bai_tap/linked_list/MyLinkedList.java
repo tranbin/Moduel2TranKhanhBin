80603b9bf019b70a793f3dc763004d2e5ed80d0c
tranbin <trankhanhbin1271997@gmail.com>
2021-10-22 23:18:17 +0700

    bo sung bt

--- src/ss9_dsa_collection_linkedlist_set/bai_tap/linked_list/MyLinkedList.java
+++ src/ss9_dsa_collection_linkedlist_set/bai_tap/linked_list/MyLinkedList.java
@@ -65,13 +65,105 @@ public class MyLinkedList<E> {
         // tăng số lượng phần tử trong danh sách lên 1
         numNodes++;
     }
-    public Object get(int index){
+
+    public Object get(int index) {
         //Khai báo biến temp trỏ tới head
         Node temp = head;
         for (int i = 0; i < index; i++) {
-                temp = temp.next;
+            temp = temp.next;
         }
         return temp.data;
+    }
+
+    public int size() {
+        return numNodes;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > numNodes) {
+            throw new IllegalArgumentException("Error index : " + index);
+        }
+        // khai báo biến temp trỏ đến head
+        Node temp = head;
+        Object data;
+
+        // Nếu index = 0 thì sẽ trả về data hiện tại
+        if (index == 0) {
+            data = temp.data;
+            head = head.next;
+            numNodes--;
+        } else {
+            for (int i = 0; i < index - 1 && temp.next != null; i++) {
+                temp = temp.next;
+            }
+            data = temp.next.data;
+            temp.next = temp.next.next;
+            numNodes--;
+
+        }
+        return (E) data;
+    }
+
+    public boolean remove(E element) {
+        // remove đối tượng nếu element head
+        if (head.data.equals(element)) {
+            remove(0);
+            return true;
+        } else {
+            //Khai báo 1 node temp trỏ đến head
+            Node temp = head;
+            while (temp.next != null) {
+                // nếu như tồn tại 1 pt có data bằng data truyền vào thì node đó sẽ bị remove
+                if (temp.next.data.equals(element)) {
+                    temp.next = temp.next.next;
+                    numNodes--;
+                    return true;
+                }
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
+
+    public MyLinkedList<E> clone() {
+        // Kiểm tra LinkedList có phần tử hay không
+        if (numNodes == 0) {
+            throw new NullPointerException("LinkedList is null");
+        }
+        // khai báo linkedlist trả về
+        MyLinkedList<E> returnLinkedList = new MyLinkedList<>();
+        //KHAI BÁO 1 TEMP TRỎ ĐẾN HEAD
+        Node temp = head;
+//add temp vào danh sách trả về để nó làm head
+        returnLinkedList.addFirst((E) temp.data);
+        temp = temp.next;
+        while (temp != null) {
+            returnLinkedList.addLast((E) temp.data);
+            temp = temp.next;
+        }
+        return returnLinkedList;
+    }
+
+    public boolean constrains(E element) {
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.data.equals(element)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public int indexOf(E element) {
+        Node temp = head;
+        for (int i = 0; i < numNodes; i++) {
+            if (temp.getData().equals(element)) {
+                return i;
+            }
+            temp= temp.next;
+        }
+        return -1;
     }
 }
 
--- src/ss9_dsa_collection_linkedlist_set/bai_tap/linked_list/TestMyLinkedList.java
+++ src/ss9_dsa_collection_linkedlist_set/bai_tap/linked_list/TestMyLinkedList.java
@@ -1,33 +1,64 @@
 package ss9_dsa_collection_linkedlist_set.bai_tap.linked_list;
 
 public class TestMyLinkedList {
-    class Student{
-        private int id ;
-        private String name;
-        public Student(){
-        }
+    public static void main(String[] args) {
+        class Student {
+            private int id;
+            private String name;
 
-        public Student(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+            public Student() {
+            }
 
-        public int getId() {
-            return id;
-        }
+            public Student(int id, String name) {
+                this.id = id;
+                this.name = name;
+            }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+            public int getId() {
+                return id;
+            }
 
-        public String getName() {
-            return name;
-        }
+            public void setId(int id) {
+                this.id = id;
+            }
 
-        public void setName(String name) {
-            this.name = name;
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
         }
+        MyLinkedList<Student> myLinkedList = new MyLinkedList<>();
+        Student student1 = new Student(1, "Bin");
+        Student student2 = new Student(2, "Nam");
+        Student student3 = new Student(3, "Trung");
+        Student student4 = new Student(3, "Trung");
+        Student student5 = new Student(4, "Toan");
+
+        myLinkedList.addFirst(student1);
+        myLinkedList.addFirst(student2);
+        myLinkedList.addLast(student3);
+        myLinkedList.add(2, student4);
+        System.out.println(myLinkedList.indexOf(student3));
+       // System.out.println(myLinkedList.constrains(student5));
+
+        //myLinkedList.remove(2);
+        //    myLinkedList.remove(student2);
+
+        /* MyLinkedList<Student> cloneLinkedList = myLinkedList.clone();*/
+       /* for (int i = 0; i < cloneLinkedList.size(); i++) {
+            Student student = (Student) cloneLinkedList.get(i);  // hiển thị
+            System.out.println(student.getName());
+*/
+        /*for (int i = 0; i < myLinkedList.size(); i++) {
+            Student student = (Student) myLinkedList.get(i);  // hiển thị
+            System.out.println(student.getName());
+        }
+*/
+
     }
-    MyLinkedList<Student> myLinkedList = new MyLinkedList<>();
-
 }
+
+
