b9a936bd7a485c01665aac4fc927755973784514
tranbin <trankhanhbin1271997@gmail.com>
2021-10-28 00:33:21 +0700

    up bt

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,12 +2,21 @@ package Test;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
-    Convert c = new Convert();
-    int num = 123;
-       c.convert(num);
+        Stack<Integer> stacks = new Stack<>();
+        stacks.push(1);
+        stacks.push(3);
+        stacks.push(4);
+        System.out.println(stacks);
+//        System.out.println(stacks.pop());  // 4
+//        System.out.println(stacks.pop());  // 3
+//        System.out.println(stacks.pop());  // 1
 
+        while (!stacks.isEmpty()){
+            System.out.println("phần tử: "+ stacks.pop());
+        }
     }
 }
--- src/ss11_dsa_queu_stack/bai_tap/Bai04KiemTraDoiXungTrongNgoac.java
+++ src/ss11_dsa_queu_stack/bai_tap/Bai04KiemTraDoiXungTrongNgoac.java
@@ -1,10 +1,38 @@
 package ss11_dsa_queu_stack.bai_tap;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Bai04KiemTraDoiXungTrongNgoac {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào biểu thức bạn cần kiểm tra");
+        Stack<Character> stacks = new Stack<>();
+        Scanner input  = new Scanner(System.in);
+        boolean check = true;
+        char left = ' ';
+        System.out.println("Ban hay nhap bieu thuc kiem tra!!!");
+        String chars = input.nextLine();
+        char arrs[] = chars.toCharArray();
+        for(int i = 0;i<arrs.length;i++){
+            if(arrs[i]=='('){
+                stacks.add(arrs[i]);
+            }
+            if(arrs[i] ==')'){
+                if(stacks.isEmpty()){
+                    check = false;
+                    break;
+                }
+                left = stacks.pop();
+                if(left!='('){
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if(stacks.isEmpty()&&check){
+            System.out.println("Su dung dung dau ngoac!!!!!");
+        }
+        else {
+            System.out.println("Su dung sai dau ngoac!!!!!!");
+        }
     }
 }
