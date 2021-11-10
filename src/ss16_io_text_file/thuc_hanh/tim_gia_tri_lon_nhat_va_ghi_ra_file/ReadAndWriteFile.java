6b3614ae43e1fcb39b3945080d4bc4c0f7fe68a3
tranbin <trankhanhbin1271997@gmail.com>
2021-11-10 18:22:43 +0700

    update bai thuc hanh

--- src/ss16_io_text_file/thuc_hanh/tim_gia_tri_lon_nhat_va_ghi_ra_file/ReadAndWriteFile.java
+++ src/ss16_io_text_file/thuc_hanh/tim_gia_tri_lon_nhat_va_ghi_ra_file/ReadAndWriteFile.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadAndWriteFile {
-    public List<Integer> readFile(String filePath){
-        filePath = "ss16_io_text_file/thuc_hanh/tim_gia_tri_lon_nhat_va_ghi_ra_file/file.txt";
+    public List<Integer> readFile(String file_Path){
         List<Integer> numbers = new ArrayList<>();
         try {
-            File file = new File(filePath);
+            File file = new File(file_Path);
 
             if (!file.exists()) {
                 throw new FileNotFoundException();
@@ -22,14 +21,14 @@ public class ReadAndWriteFile {
             }
             br.close();
         } catch (Exception e) {
-            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+            System.err.println("File không tồn tại or nội dung có lỗi!");
         }
         return numbers;
     }
 
-    public void writeFile(String filePath, int max){
+    public void writeFile(String file_Path, int max){
         try {
-            FileWriter writer = new FileWriter(filePath, true);
+            FileWriter writer = new FileWriter(file_Path, true);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
             bufferedWriter.write("Giá trị lớn nhất là: " + max);
             bufferedWriter.close();
